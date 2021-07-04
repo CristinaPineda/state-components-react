626db9aab24205fe6f4af544f35427e079c636f9
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-07-04 17:49:18 -0300

    primeiras mudanças

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+// import colors from './data';
 
 function App() {
+  // constructor() {
+  //   super();
+
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Colors Selector</p>
+        <hr className="hr"></hr>
       </header>
+      {/* <div className="Select-colors">
+      <label htmlFor="Label-list">
+        Enter a Color
+          <input id="Label-list" type="text" onChange={() => {}}/>
+        </label>
+        <ul className="Color-list">
+          {colors.map((color) => <li className="Color-item" key={color.value}>{color.color} - {color.value}</li>)}
+        </ul>
+      </div> */}
     </div>
   );
 }
