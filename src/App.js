4306e247e2ea6c2a6aa3db2eb17fc1b252a01030
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-07-04 17:59:01 -0300

    declara classe e define status

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import colors from './data';
+import React from 'react';
 
-function App() {
-  // constructor() {
-  //   super();
+class App extends React.Component {
+  constructor() {
+    super();
 
-  // }
+    this.state ={
+      inputValue: '',
+    }
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Colors Selector</p>
-        <hr className="hr"></hr>
-      </header>
-      <div className="Select-colors">
-      {/* <label htmlFor="Label-list">
-        Enter a Color
-          <input id="Label-list" type="text" onChange={() => {}}/>
-        </label> */}
-        <ul className="Color-list">
-          {colors.map((color) => <li className="Color-item" key={color.value}>{color.color} - {color.value}</li>)}
-        </ul>
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Colors Selector</p>
+          <hr className="hr" />
+        </header>
+        <div className="Select-colors">
+          <label htmlFor="Label-list">
+            Enter a Color
+            <input id="Label-list" type="text" onChange={() => {}}/>
+          </label>
+          <ul className="Color-list">
+            {colors.map((color) => <li className="Color-item" key={color.value}>{color.color} - {color.value}</li>)}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
